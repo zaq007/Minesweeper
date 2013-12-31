4a62f83382779ff68241ff8b4a656085d4c7c3a3
zaq007 <sergey4000@tut.by>
2013-12-31 12:14:58 +0300

    No shaders(?); Deleted unusable files;

--- Core/Core/Core.cs
+++ Core/Core/Core.cs
@@ -13,16 +13,12 @@ namespace Core
     {
         Controller controller;
         Game Game;
-        RenderTarget2D target;
-        RenderTarget2D shader;
 
         public Core(Game game, SpriteBatch spriteBatch, int x, int y, int n)
         {
             Player.MinesNumber = n;
             controller = new Controller(x, y, n);
             Game = game;
-            target = new RenderTarget2D(game.GraphicsDevice, game.Window.ClientBounds.Width, game.Window.ClientBounds.Height);
-            shader = new RenderTarget2D(game.GraphicsDevice, game.Window.ClientBounds.Width, game.Window.ClientBounds.Height);
         }
 
         public string Update(GameTime gameTime)
@@ -34,11 +30,8 @@ namespace Core
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            //Game.GraphicsDevice.SetRenderTarget(target);
-            //Game.GraphicsDevice.Clear(Color.Black);
-            spriteBatch.Begin(SpriteSortMode.Immediate, null, null, null, null, TextureLoader.Shader);
-            controller.Draw(spriteBatch);
-            
+            spriteBatch.Begin();
+            controller.Draw(spriteBatch);            
             spriteBatch.End();
         }
     }
